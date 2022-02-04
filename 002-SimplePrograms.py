1dfc48bcce5ca0d0fab88033d90669bf2d81dbf2
SilviuDN <silviudilimot@gmail.com>
2022-02-04 05:55:59 +0100

    Testing systemdate

---
+++ 002-SimplePrograms.py
@@ -0,0 +1,11 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Fri Feb  4 19:42:35 2022
+
+@author: Silviu
+"""
+
+#Exercise "Guess my number"
+
+secretNumber = input("Please think of a number between 0 and 100! ")
+print(secretNumber)
