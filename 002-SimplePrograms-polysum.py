6c10b03b3bcb223ba2a8a13b7593fdf1cef1b5e7
SilviuDN <silviudilimot@gmail.com>
2022-02-07 22:53:58 +0100

    End of the year ballance problem

--- 002-SimplePrograms-polysum.py
+++ 002-SimplePrograms-polysum.py
@@ -2,15 +2,61 @@
 """
 Created on Sat Feb  5 18:02:47 2022
 
-@author: User
+@author: Silviu
+
 """
 import math
 
 def polysum(n, s):
+    '''
+    Parameters
+    ----------
+    n : int
+        number of sides of a regular polygon
+        
+    s : float
+        side length of a regular polygon
+
+    Returns
+    -------
+    TYPE: float
+        sum of square of the perimeter with the area of the regular polygon, rounded to 4 decimals
+
+    '''
     return round( area(n, s) + perimeter(n, s)**2 , 4 )
 
 def area(n, s):
+    '''
+    Parameters
+    ----------
+    n : int
+        number of sides of a regular polygon
+        
+    s : float
+        side length of a regular polygon
+
+    Returns
+    -------
+    TYPE: float
+        area of the regular polygon.
+
+    '''
     return 0.25*n*s*s/math.tan(math.pi/n)
 
 def perimeter(n, s):
+    '''
+    Parameters
+    ----------
+    n : int
+        number of sides of a regular polygon
+        
+    s : float
+        side length of a regular polygon
+
+    Returns
+    -------
+    TYPE: float
+        perimeter of the regular polygon
+
+    '''
     return n*s
