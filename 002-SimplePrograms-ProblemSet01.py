93928af28ede12be5b91f0eaf545cbb933ac5eb2
SilviuDN <silviudilimot@gmail.com>
2022-02-07 23:53:56 +0100

    End of the year ballance problem

---
+++ 002-SimplePrograms-ProblemSet01.py
@@ -0,0 +1,18 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Mon Feb  7 22:21:38 2022
+
+@author: Silviu
+"""
+month = 12
+balance = 42
+annualInterestRate = 0.2
+monthlyPaymentRate = 0.04
+          
+          
+def remainingBalance(balance, annualInterestRate, monthlyPaymentRate, month):
+    if month == 0: return balance
+    return remainingBalance(balance, annualInterestRate, monthlyPaymentRate, month-1) * (1 - monthlyPaymentRate) * ( 1 + annualInterestRate / 12.0 )
+
+
+print('Remaining balance: ' + str( round( remainingBalance(balance, annualInterestRate, monthlyPaymentRate, 12) , 2) ))
