8e3254149dd1d59cf8c605f881bb4ee15e3d308f
SilviuDN <silviudilimot@gmail.com>
2022-02-16 14:40:39 +0100

    Swapping with tuples

---
+++ 003-Tuples-swap.py
@@ -0,0 +1,14 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Thu Feb 17 14:35:36 2022
+
+@author: Silviu
+"""
+
+num = 2
+word = 'two'
+
+(num, word) = (word, num)
+
+print('numarul e ', num)
+print('Se citeste ', word)
